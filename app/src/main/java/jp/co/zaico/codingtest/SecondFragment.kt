f16aba8ed11a857067239f126969d986fae4e1aa
t-saita417 <takaaki.saita@gmail.com>
2025-08-23 13:22:37 +0900

    SecondFragmentのState定義、データの渡し方など整理

--- app/src/main/java/jp/co/zaico/codingtest/SecondFragment.kt
+++ app/src/main/java/jp/co/zaico/codingtest/SecondFragment.kt
@@ -1,18 +1,25 @@
 package jp.co.zaico.codingtest
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.zaico.codingtest.databinding.FragmentSecondBinding
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private val binding
+        get() = _binding!!
+
     private val viewModel: SecondViewModel by viewModels()
 
     override fun onCreateView(
@@ -20,24 +27,57 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return _binding!!.root
-
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val inventoryId = requireArguments().getString("inventoryId")!!.toInt()
-
-        val inventory = viewModel.getInventory(inventoryId)
-        initView(inventory)
-
+        observeViewModel()
     }
 
-    private fun initView(inventory: Inventory) {
-        _binding!!.textViewId.text = inventory.id.toString()
-        _binding!!.textViewTitle.text = inventory.title
-        _binding!!.textViewQuantity.text = inventory.quantity
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    when (uiState) {
+                        SecondViewModel.UiState.Initial -> {
+                            initView()
+                            requireArguments().getString("inventoryId")?.toIntOrNull()?.let { id ->
+                                viewModel.getInventory(id)
+                            } ?: viewModel.setUiStateError()
+                        }
+
+                        SecondViewModel.UiState.Loading -> {
+                            // TODO:読み込み中表示
+                        }
+
+                        is SecondViewModel.UiState.DataFetched -> {
+                            updateView(uiState.data)
+                        }
+
+                        is SecondViewModel.UiState.Error -> {
+                            // TODO:データ取得エラー表示
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initView() {
+        binding.textViewId.text = ""
+        binding.textViewTitle.text = ""
+        binding.textViewQuantity.text = ""
+    }
+
+    private fun updateView(inventory: Inventory) {
+        binding.textViewId.text = inventory.id.toString()
+        binding.textViewTitle.text = inventory.title
+        binding.textViewQuantity.text = inventory.quantity
+    }
 }
--- app/src/main/java/jp/co/zaico/codingtest/SecondViewModel.kt
+++ app/src/main/java/jp/co/zaico/codingtest/SecondViewModel.kt
@@ -1,12 +1,12 @@
 package jp.co.zaico.codingtest
 
-import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.zaico.codingtest.core.data.ZaicoRepository
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,13 +14,31 @@ class SecondViewModel @Inject constructor(
     val zaicoRepository: ZaicoRepository
 ) : ViewModel() {
 
-    // データ取得
-    @SuppressLint("DefaultLocale")
-    fun getInventory(inventoryId: Int): Inventory = runBlocking {
-        return@runBlocking GlobalScope.async {
-            zaicoRepository.getInventory(inventoryId)
-        }.await()
-
+    sealed interface UiState {
+        data object Initial : UiState
+        data object Loading : UiState
+        data class DataFetched(val data: Inventory) : UiState
+        data class Error(val e: Exception?) : UiState
     }
 
+    private val _uiState = MutableStateFlow<UiState>(UiState.Initial)
+    val uiState = _uiState.asStateFlow()
+
+    // データ取得
+    fun getInventory(inventoryId: Int) {
+        _uiState.value = UiState.Loading
+        viewModelScope.launch {
+            val result = zaicoRepository.getInventory(inventoryId)
+            // TODO:resultのエラーチェック。Result型定義して返却するようにしたい
+//            if (result is Result.Success) {
+                _uiState.value = UiState.DataFetched(result)
+//            } else {
+//                _uiState.value = UiState.Error(result.e)
+//            }
+        }
+    }
+
+    fun setUiStateError(e: Exception? = null) {
+        _uiState.value = UiState.Error(e)
+    }
 }
