537fb2275877c35f4d0f9471c64ed06f5e058d6d
t-saita417 <takaaki.saita@gmail.com>
2025-08-23 14:00:57 +0900

    FirstViewModelのState定義、データの渡し方など整理

--- app/src/main/java/jp/co/zaico/codingtest/FirstFragment.kt
+++ app/src/main/java/jp/co/zaico/codingtest/FirstFragment.kt
@@ -8,6 +8,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -16,60 +19,97 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.zaico.codingtest.databinding.FragmentFirstBinding
+import kotlinx.coroutines.launch
 import kotlin.getValue
 
 @AndroidEntryPoint
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private val binding
+        get() = _binding!!
 
-    private val viewmodel: FirstViewModel by viewModels()
+    private val viewModel: FirstViewModel by viewModels()
+
+    private val adapter = MyAdapter(object : MyAdapter.OnItemClickListener {
+        override fun itemClick(item: Inventory) {
+            val bundle = bundleOf("inventoryId" to item.id.toString())
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
+        }
+    })
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFirstBinding.inflate(layoutInflater)
-        return _binding!!.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val _layoutManager = LinearLayoutManager(context!!)
-        val _dividerItemDecoration = DividerItemDecoration(
-            context!!,
-            _layoutManager.orientation
-        )
-        val _adapter = MyAdapter(object : MyAdapter.OnItemClickListener {
-            override fun itemClick(item: Inventory) {
-                val bundle = bundleOf("inventoryId" to item.id.toString())
-                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
-            }
-        })
-
-        _binding!!.recyclerView.also {
-            it.layoutManager = _layoutManager
-            it.addItemDecoration(_dividerItemDecoration)
-            it.adapter = _adapter
-        }
-
-        viewmodel.getInventories().apply {
-            _adapter.submitList(this)
-        }
-
+        observeViewModel()
     }
 
+    override fun onResume() {
+        super.onResume()
+        // TODO:詳細画面からの戻り時にデータ取得できるようInitialに戻す。毎回取るのも微妙なのでキャッシュで表示するなど後で検討する
+        viewModel.setUiStateInitial()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    when (uiState) {
+                        FirstViewModel.UiState.Initial -> {
+                            initView()
+                            viewModel.getInventories()
+                        }
+
+                        FirstViewModel.UiState.Loading -> {
+                            // TODO:読み込み中表示
+                        }
+
+                        is FirstViewModel.UiState.DataFetched -> {
+                            adapter.submitList(uiState.data)
+                        }
+
+                        is FirstViewModel.UiState.Error -> {
+                            // TODO:データ取得エラー表示
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initView() {
+        val layoutManager = LinearLayoutManager(requireContext())
+        val dividerItemDecoration = DividerItemDecoration(
+            requireContext(),
+            layoutManager.orientation
+        )
+        binding.recyclerView.also {
+            it.layoutManager = layoutManager
+            it.addItemDecoration(dividerItemDecoration)
+            it.adapter = adapter
+        }
+    }
 }
 
-val diff_util= object: DiffUtil.ItemCallback<Inventory>(){
-    override fun areItemsTheSame(oldItem: Inventory, newItem: Inventory): Boolean
-    {
-        return oldItem.title== newItem.title
+val diff_util = object : DiffUtil.ItemCallback<Inventory>() {
+    override fun areItemsTheSame(oldItem: Inventory, newItem: Inventory): Boolean {
+        return oldItem.title == newItem.title
     }
 
-    override fun areContentsTheSame(oldItem: Inventory, newItem: Inventory): Boolean
-    {
-        return oldItem== newItem
+    override fun areContentsTheSame(oldItem: Inventory, newItem: Inventory): Boolean {
+        return oldItem == newItem
     }
 
 }
@@ -78,26 +118,24 @@ class MyAdapter(
     private val itemClickListener: OnItemClickListener,
 ) : ListAdapter<Inventory, MyAdapter.ViewHolder>(diff_util) {
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-    interface OnItemClickListener{
+    interface OnItemClickListener {
         fun itemClick(item: Inventory)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-    {
-        val _view= LayoutInflater.from(parent.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val _view = LayoutInflater.from(parent.context)
             .inflate(R.layout.first_item, parent, false)
         return ViewHolder(_view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int)
-    {
-        val _item= getItem(position)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val _item = getItem(position)
         (holder.itemView.findViewById<View>(R.id.textView_id) as TextView).text = _item.id.toString()
         (holder.itemView.findViewById<View>(R.id.textView_title) as TextView).text = _item.title
 
-        holder.itemView.setOnClickListener{
+        holder.itemView.setOnClickListener {
             itemClickListener.itemClick(_item)
         }
     }
--- app/src/main/java/jp/co/zaico/codingtest/FirstViewModel.kt
+++ app/src/main/java/jp/co/zaico/codingtest/FirstViewModel.kt
@@ -1,11 +1,12 @@
 package jp.co.zaico.codingtest
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.zaico.codingtest.core.data.ZaicoRepository
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,12 +14,30 @@ class FirstViewModel @Inject constructor(
     val zaicoRepository: ZaicoRepository
 ) : ViewModel() {
 
-    // データ取得
-    fun getInventories(): List<Inventory> = runBlocking {
-        // TODO:UiState定義してView側に通知する
-        return@runBlocking GlobalScope.async {
-            return@async zaicoRepository.getInventories()
-        }.await()
+    sealed interface UiState {
+        data object Initial : UiState
+        data object Loading : UiState
+        data class DataFetched(val data: List<Inventory>) : UiState
+        data class Error(val e: Exception?) : UiState
     }
 
+    private val _uiState = MutableStateFlow<UiState>(UiState.Initial)
+    val uiState = _uiState.asStateFlow()
+
+    fun getInventories() {
+        _uiState.value = UiState.Loading
+        viewModelScope.launch {
+            val result = zaicoRepository.getInventories()
+            // TODO:resultのエラーチェック。Result型定義して返却するようにしたい
+//            if (result is Result.Success) {
+            _uiState.value = UiState.DataFetched(result)
+//            } else {
+//                _uiState.value = UiState.Error(result.e)
+//            }
+        }
+    }
+
+    fun setUiStateInitial() {
+        _uiState.value = UiState.Initial
+    }
 }
